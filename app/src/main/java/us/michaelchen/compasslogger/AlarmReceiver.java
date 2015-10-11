9679bd30681322676843d5cf8979d94aea9c132e
Michael Chen <mc.michaelchen.us@gmail.com>
2015-10-10 20:33:33 -0700

    Added GPS

--- app/src/main/java/us/michaelchen/compasslogger/AlarmReceiver.java
+++ app/src/main/java/us/michaelchen/compasslogger/AlarmReceiver.java
@@ -1,16 +1,24 @@
 package us.michaelchen.compasslogger;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
 import com.firebase.client.Firebase;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -26,20 +34,25 @@ public class AlarmReceiver extends BroadcastReceiver {
     private SensorManager sensorManager;
     private Firebase firebase;
     private String deviceId;
+    private Context context;
 
     public static final String ROTATION_KEY = "Rotation";
     public static final String AZIMUTH_KEY = "Azimuth";
-    public AlarmReceiver() {
-    }
+    public static final String USER_DATA_KEY = "userData";
+    public static final String LOCATION_KEY = "location";
+    public static final String LATITUDE_KEY = "latitude";
+    public static final String LONGITUDE_KEY = "longitude";
+    public static final String GPS_KEY = "gps";
 
     @Override
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
 //        throw new UnsupportedOperationException("Not yet implemented");
+        this.context = context;
         Firebase.setAndroidContext(context);
         firebase = new Firebase("https://luminous-torch-7892.firebaseio.com/");
-        deviceId = getDeviceId(context);
+        deviceId = getDeviceId();
 
         sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
         List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ROTATION_VECTOR);
@@ -48,7 +61,7 @@ public class AlarmReceiver extends BroadcastReceiver {
         }
     }
 
-    public String getDeviceId(Context context) {
+    public String getDeviceId() {
         final TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
 
         final String tmDevice, tmSerial, androidId;
@@ -75,12 +88,13 @@ public class AlarmReceiver extends BroadcastReceiver {
             sensorManager.unregisterListener(this);
             float[] rMat = new float[9];
             float[] orientation = new float[3];
-            SensorManager.getRotationMatrixFromVector( rMat, event.values );
+            SensorManager.getRotationMatrixFromVector(rMat, event.values);
             // get the azimuth value (orientation[0]) in degree
             int azimuth = (int) ( Math.toDegrees( SensorManager.getOrientation( rMat, orientation )[0] ) + 360 ) % 360;
 
             Log.d(TAG, event.toString());
-            Firebase dataRef = firebase.child("usersData").child(deviceId);
+            Firebase dataRef = firebase.child(USER_DATA_KEY).child(deviceId);
+            Firebase locationRef = dataRef.child(LOCATION_KEY);
 
             List<Float> values = new ArrayList<>(event.values.length);
             for (int i = 0; i < event.values.length; i++) {
@@ -90,8 +104,27 @@ public class AlarmReceiver extends BroadcastReceiver {
             Map<String, Object> map = new HashMap<>();
             map.put(AZIMUTH_KEY, azimuth);
             map.put(ROTATION_KEY, values);
+            String locationProvider = LocationManager.GPS_PROVIDER;
+            int permissionCheck = ContextCompat.checkSelfPermission(context,
+                    Manifest.permission.ACCESS_FINE_LOCATION);
+            if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+                LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+                Location location = locationManager.getLastKnownLocation(locationProvider);
+                HashMap<String, Double> coords = new HashMap<>();
+                if (location == null) {
+                    location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                }
+                if (location != null) {
+                    coords.put(LATITUDE_KEY, location.getLatitude());
+                    coords.put(LONGITUDE_KEY, location.getLongitude());
+                }
+                map.put(GPS_KEY, coords);
+            } else {
+                // Get coarse location?
+            }
 
-            dataRef.push().setValue(map);
+
+            locationRef.push().setValue(map);
 //            firebase.child("Message").setValue(values);
         }
     };
--- app/src/main/java/us/michaelchen/compasslogger/MainActivity.java
+++ app/src/main/java/us/michaelchen/compasslogger/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.net.Uri;
+import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
