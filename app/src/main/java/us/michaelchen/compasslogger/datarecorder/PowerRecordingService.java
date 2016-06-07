999d502a62c38df32a096bae59ddf669f6ff6da2
ioreyes <ioreyes@icsi.berkeley.edu>
2016-06-07 15:18:41 -0700

    Collecting charge, temperature, and voltage battery data

--- app/src/main/java/us/michaelchen/compasslogger/datarecorder/PowerRecordingService.java
+++ app/src/main/java/us/michaelchen/compasslogger/datarecorder/PowerRecordingService.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.BatteryManager;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
@@ -15,6 +15,9 @@ public class PowerRecordingService extends AbstractRecordingService {
     private static final String CHARGING_KEY = "charging";
     private static final String CHARGING_USB_KEY = "usbCharging";
     private static final String CHARGING_AC_KEY = "acCharging";
+    private static final String CHARGE_LEVEL_KEY = "chargeLevel";
+    private static final String TEMPERATURE_KEY = "temperature";
+    private static final String VOLTAGE_KEY = "voltage";
 
     public PowerRecordingService() {
         super("PowerRecordingService");
@@ -32,6 +35,13 @@ public class PowerRecordingService extends AbstractRecordingService {
         int statusCode = batteryStatus.getIntExtra(BatteryManager.EXTRA_STATUS, -1);
         int chargePlug = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
 
+        int currentChargeLevel = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int maxChargeLevel = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+        String charge = String.format("%d / %d", currentChargeLevel, maxChargeLevel);
+
+        int temperature = batteryStatus.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, -1);
+        int voltage = batteryStatus.getIntExtra(BatteryManager.EXTRA_VOLTAGE, -1);
+
         // Figure out the charging state
         boolean isCharging = statusCode == BatteryManager.BATTERY_STATUS_CHARGING ||
                              statusCode == BatteryManager.BATTERY_STATUS_FULL;
@@ -39,10 +49,13 @@ public class PowerRecordingService extends AbstractRecordingService {
         boolean onAC = chargePlug == BatteryManager.BATTERY_PLUGGED_AC;
 
         // Record the data
-        Map<String, Object> data = new HashMap<>();
+        Map<String, Object> data = new LinkedHashMap<>();
         data.put(CHARGING_KEY, isCharging);
         data.put(CHARGING_USB_KEY, onUSB);
         data.put(CHARGING_AC_KEY, onAC);
+        data.put(CHARGE_LEVEL_KEY, charge);
+        data.put(TEMPERATURE_KEY, temperature);
+        data.put(VOLTAGE_KEY, voltage);
 
         return data;
     }
