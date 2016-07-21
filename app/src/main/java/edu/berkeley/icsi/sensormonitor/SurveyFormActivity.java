6c7ae774e318357416dfccd3e6d321effb628256
Manar <safi.manar@berkeley.edu>
2016-07-21 15:27:32 -0700

    UI: In-app Survey form working; implemented surveyform and survey redirect activities

--- app/src/main/java/edu/berkeley/icsi/sensormonitor/SurveyFormActivity.java
+++ app/src/main/java/edu/berkeley/icsi/sensormonitor/SurveyFormActivity.java
@@ -1,7 +1,14 @@
 package edu.berkeley.icsi.sensormonitor;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import edu.berkeley.icsi.sensormonitor.utils.PreferencesWrapper;
 
 public class SurveyFormActivity extends AppCompatActivity {
 
@@ -9,5 +16,44 @@ public class SurveyFormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey_form);
+
+        setWebView(this);
     }
+
+    /*Sets up the WebView in the layout to show the online survey page. */
+    private void setWebView(Context context) {
+        String deviceId = PreferencesWrapper.getDeviceID();
+        String formURL = getResources().getString(R.string.gdocs_url);
+
+        WebViewClient webViewClient = getWebviewClient(context);
+        WebView webView = (WebView) findViewById(R.id.survey_form_view);
+        webView.setWebViewClient(webViewClient);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(formURL + deviceId);
+    }
+
+
+    /**
+     * Custom web client to let us override behaviour when the Google form has been submitted
+     */
+    private WebViewClient getWebviewClient(final Context context) {
+        return new WebViewClient() {
+
+            /**
+             * Broadcast a survey-complete intent once the form has been submitted, to be
+             * received by the surveyReceiver BroadCastReceiver in MainActivity.
+             */
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                if (url.contains(getString(R.string.survey_form_response))) {
+                    Intent intent = new Intent("survey-complete");
+                    LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
+                }
+            }
+
+
+        };
+    }
+
 }
--- app/src/main/java/edu/berkeley/icsi/sensormonitor/SurveyRedirectActivity.java
+++ app/src/main/java/edu/berkeley/icsi/sensormonitor/SurveyRedirectActivity.java
@@ -1,7 +1,11 @@
 package edu.berkeley.icsi.sensormonitor;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SurveyRedirectActivity extends AppCompatActivity {
 
@@ -9,6 +13,20 @@ public class SurveyRedirectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey_redirect);
+
+        programButtons(this);
+    }
+
+    private void programButtons(final Context context) {
+
+        final Button agreeButton = (Button) findViewById(R.id.survey_continue);
+        agreeButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Show the Survey Redirect Activity
+                Intent intent = new Intent(context, SurveyFormActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
