561fd5210becfa90b48795e8de812a4e1b36c1e3
Manar <safi.manar@berkeley.edu>
2016-07-21 16:46:01 -0700

    UI: Implemented Info activity with dynamic info replacement

--- app/src/main/java/edu/berkeley/icsi/sensormonitor/InfoActivity.java
+++ app/src/main/java/edu/berkeley/icsi/sensormonitor/InfoActivity.java
@@ -2,6 +2,9 @@ package edu.berkeley.icsi.sensormonitor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import edu.berkeley.icsi.sensormonitor.utils.PreferencesWrapper;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -9,5 +12,54 @@ public class InfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        updateInfo();
     }
+
+
+    /**
+     * Updates the textView objects (Verification Code, ID, and Build)
+     * on the InfoActivity layout to display the proper information.
+     */
+    private void updateInfo() {
+        updateVerification();
+        updateUUID();
+        updateBuild();
+    }
+
+
+    private void updateVerification() {
+        TextView verifView= (TextView) findViewById(R.id.info_verif_code);
+
+        String verif = "12345678";
+
+        String text = getString(R.string.info_verif_code);
+        // Concat the verification code
+        text = text + " " + verif;
+        verifView.setText(text);
+    }
+
+    private void updateUUID() {
+        TextView uuidView= (TextView) findViewById(R.id.info_id);
+
+        String uuid = PreferencesWrapper.getDeviceID();
+
+        String text = getString(R.string.info_id);
+        // Concat the uuid
+        text = text + " " + uuid;
+        uuidView.setText(text);
+    }
+
+
+    private void updateBuild() {
+        TextView buildView= (TextView) findViewById(R.id.info_build);
+
+        String build = getString(R.string.app_version);
+
+        String text = getString(R.string.info_build);
+        // Concat the build
+        text = text + " " + build;
+        buildView.setText(text);
+    }
+
 }
