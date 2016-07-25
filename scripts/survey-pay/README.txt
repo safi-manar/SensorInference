6873e42c1766e1aab7b934ce6a86dc25973c28c9
ioreyes <ioreyes@icsi.berkeley.edu>
2016-07-24 18:14:37 -0700

    Mark processed daily surveys

--- scripts/survey-pay/README.txt
+++ scripts/survey-pay/README.txt
@@ -4,3 +4,5 @@ REQUIRES
 - Python 2.7.x
 - SurveyGizmo module (https://github.com/ITNG/SurveyGizmo/)
 
+SURVEY ASSUMPTIONS
+- Surveys contain "uuid" and "processed" hidden actions
--- scripts/survey-pay/survey-pay.py
+++ scripts/survey-pay/survey-pay.py
@@ -4,30 +4,72 @@ import json
 from surveygizmo import SurveyGizmo
 
 def parse_args():
+    """Specify a parser for a single mandatory command-line argument"""
     parser = argparse.ArgumentParser(description='Examine survey submissions and pay participants')
-    parser.add_argument('apiPath', help='path to api.secret file')
+    parser.add_argument('api_path', help='path to api.secret file')
 
     return parser.parse_args()
 
 def read_json(path):
+    """Return a dict representation of a JSON file"""
     json_data = open(path).read()
     return json.loads(json_data)
 
+def get_valid_uuids(api_secrets):
+    return
+
+def process_survey_gizmo(api_secrets):
+    client = SurveyGizmo(
+        api_version = api_secrets['sm_api_version'],
+        api_token = api_secrets['sm_api_key'],
+        api_token_secret = api_secrets['sm_api_secret']
+    )
+
+    # Identify surveys that belong to this project
+    surveys = client.api.survey.list()
+    survey_data = surveys['data']
+    entry_survey_id = [survey['id'] for survey in survey_data if survey['title'] == api_secrets['sm_entry_survey_title']][0]
+    daily_survey_id = [survey['id'] for survey in survey_data if survey['title'] == api_secrets['sm_daily_survey_title']][0]
+
+    process_entry_survey(entry_survey_id, client)
+
+def process_entry_survey(entry_id, client):
+    """Return UUIDs that should be paid for submitting the entry survey. Mark those as processed."""
+    (uuid_id, processed_id) = identify_uuid_and_processed(entry_id, client)
+    uuid_field = '[question(%d), option(0)]' % uuid_id
+    processed_field = '[question(%d), option(0)]' % processed_id
+
+    responses = client.api.surveyresponse \
+                .filter('status', '!=', 'Deleted') \
+                .filter(processed_field, '=', 'false') \
+                .filter(uuid_field, 'IS NOT NULL', '') \
+                .list(entry_id)
+    uuids = [response[uuid_field] for response in responses['data']]
+
+    response_ids = [response['id'] for response in responses['data']]
+    for response_id in response_ids:
+        kwargs = {'data[%d][0]' % processed_id: 'true'}     # change the "processed" field from false to true
+        client.api.surveyresponse.update(entry_id, response_id, **kwargs)
+
+    print uuids
+
+def identify_uuid_and_processed(survey_id, client):
+    """Return the IDs for the hidden actions containing the "processed" flag and UUID """
+    uuid_id = -1
+    processed_id = -1
+
+    questions = client.api.surveyquestion.list(survey_id)
+    for question in questions['data']:
+        title = question['title']['English']
+        if title == 'uuid':
+            uuid_id = question['id']
+        elif title == 'processed':
+            processed_id = question['id']
+
+    return (uuid_id, processed_id)
+
 # Get API information
 args = parse_args()
-apiPath = args.apiPath
+api_secrets = read_json(args.api_path)
 
-apiSecret = read_json(apiPath)
-
-# Retrieve survey data
-client = SurveyGizmo(
-    api_version = apiSecret['sm_api_version'],
-    api_token = apiSecret['sm_api_key'],
-    api_token_secret = apiSecret['sm_api_secret']
-)
-
-response = client.api.survey.list()
-data = response['data']
-our_surveys = [survey for survey in data if survey['title'].lower().startswith('sensors inference')]
-
-print our_surveys
+process_survey_gizmo(api_secrets)
