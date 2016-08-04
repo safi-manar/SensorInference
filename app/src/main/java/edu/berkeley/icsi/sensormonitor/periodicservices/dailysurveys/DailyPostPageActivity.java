394e56d785c13d7bd9cc5dd5c95100d548a77560
Manar <safi.manar@berkeley.edu>
2016-08-04 09:36:54 -0700

    DailyPost layout written. (Not coded / integrated yet)

---
+++ app/src/main/java/edu/berkeley/icsi/sensormonitor/periodicservices/dailysurveys/DailyPostPageActivity.java
@@ -0,0 +1,15 @@
+package edu.berkeley.icsi.sensormonitor.periodicservices.dailysurveys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.berkeley.icsi.sensormonitor.R;
+
+public class DailyPostPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_post_page);
+    }
+}
--- app/src/main/java/edu/berkeley/icsi/sensormonitor/periodicservices/dailysurveys/DailySurveyFormActivity.java
+++ app/src/main/java/edu/berkeley/icsi/sensormonitor/periodicservices/dailysurveys/DailySurveyFormActivity.java
@@ -1,7 +1,9 @@
 package edu.berkeley.icsi.sensormonitor.periodicservices.dailysurveys;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +11,8 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import edu.berkeley.icsi.sensormonitor.R;
+import edu.berkeley.icsi.sensormonitor.periodicservices.deadline.UninstallPromptActivity;
+import edu.berkeley.icsi.sensormonitor.utils.MasterSwitch;
 import edu.berkeley.icsi.sensormonitor.utils.PreferencesWrapper;
 
 public class DailySurveyFormActivity extends AppCompatActivity {
@@ -26,6 +30,9 @@ public class DailySurveyFormActivity extends AppCompatActivity {
     private void setWebView(Context context) {
         String deviceId = PreferencesWrapper.getDeviceID();
         String formURL = getResources().getString(R.string.daily_survey_url);
+
+        // Register the daily-survey-complete receiver.
+        registerSurveyReceiver();
 
         WebViewClient webViewClient = getWebviewClient(context);
         WebView webView = (WebView) findViewById(R.id.daily_survey_view);
@@ -49,7 +56,7 @@ public class DailySurveyFormActivity extends AppCompatActivity {
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
                 if (url.contains(getString(R.string.survey_form_response))) {
-                    Intent intent = new Intent("survey-complete");
+                    Intent intent = new Intent("daily-survey-complete");
                     LocalBroadcastManager.getInstance(context).sendBroadcast(intent);
                 }
             }
@@ -57,5 +64,30 @@ public class DailySurveyFormActivity extends AppCompatActivity {
 
         };
     }
+
+
+    /**
+     * Register to receive survey completion intent.
+     * We are registering an observer (surveyReceiver) to receive Intents
+     * with actions named "daily-survey-complete".
+     */
+    private void registerSurveyReceiver() {
+        LocalBroadcastManager.getInstance(this).registerReceiver(surveyReceiver,
+                new IntentFilter("daily-survey-complete"));
+    }
+
+    /**
+     * The handler for received Intents. This will be called whenever the
+     * "daily-survey-complete" intent is broadcasted.
+     * */
+    private BroadcastReceiver surveyReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            // Now, show the user the Daily PostPage Activity.
+            Intent dailyPostIntent = new Intent(context, UninstallPromptActivity.class);
+            startActivity(dailyPostIntent);
+        }
+    };
 
 }
