2779b5241a8b6ae2be3c0d56f3333d2171ae9588
Manar <safi.manar@berkeley.edu>
2016-10-22 19:48:27 -0700

    FBLogger added deadline and daily survey logging. (not integrated)

--- app/src/main/java/edu/berkeley/icsi/sensormonitor/utils/FBLogger.java
+++ app/src/main/java/edu/berkeley/icsi/sensormonitor/utils/FBLogger.java
@@ -19,6 +19,11 @@ public class FBLogger {
     private static final String DEADLINESURVEY = "deadlinesurvey";
 
 
+    /****************************************************************************
+     *                                                                          *
+     * All log methods automatically log timestamp via FirebaseWrapper.push()   *
+     *                                                                          *
+     ****************************************************************************/
 
     public static void consentActivity() {
         Map<String, Object> data = new HashMap<>();
@@ -26,10 +31,84 @@ public class FBLogger {
         FirebaseWrapper.push(LOGS, data);
     }
 
+
+    /****************************************************************************
+     * Logging for Daily Survey                                                 *
+     ****************************************************************************/
+
+    // Log when the daily survey activity is displayed
     public static void dailySurveyActivity() {
         Map<String, Object> data = new HashMap<>();
         data.put(DAILYSURVEY, "Displayed dailysurvey activity");
         FirebaseWrapper.push(LOGS, data);
     }
+
+    // Log when the user postpones the daily survey
+    public static void dailySurveyPostponed() {
+        Map<String, Object> data = new HashMap<>();
+        data.put(DAILYSURVEY, "Postponed daily survey");
+        FirebaseWrapper.push(LOGS, data);
+    }
+
+    // Log when the user chooses to abstain from the survey
+    public static void dailySurveyAbstained() {
+        Map<String, Object> data = new HashMap<>();
+        data.put(DAILYSURVEY, "Abstained daily survey");
+        FirebaseWrapper.push(LOGS, data);
+    }
+
+    // Log when the user accepts to complete the survey (does not mean the use *has* submitted it)
+    public static void dailySurveyAccepted() {
+        Map<String, Object> data = new HashMap<>();
+        data.put(DAILYSURVEY, "Accepted daily survey");
+        FirebaseWrapper.push(LOGS, data);
+    }
+
+    // Log when the user submitted the daily survey
+    public static void dailySurveySubmitted() {
+        Map<String, Object> data = new HashMap<>();
+        data.put(DAILYSURVEY, "Submitted daily survey");
+        FirebaseWrapper.push(LOGS, data);
+    }
+
+
+    /****************************************************************************
+     * Logging for Exit (Deadline) Survey                                       *
+     ****************************************************************************/
+    // Log when the deadline activity is displayed
+    public static void deadlineActivity() {
+        Map<String, Object> data = new HashMap<>();
+        data.put(DEADLINESURVEY, "Displayed deadline activity");
+        FirebaseWrapper.push(LOGS, data);
+    }
+
+    // Log when the user postpones the deadline survey
+    public static void deadlineSurveyPostponed() {
+        Map<String, Object> data = new HashMap<>();
+        data.put(DEADLINESURVEY, "Postponed deadline survey");
+        FirebaseWrapper.push(LOGS, data);
+    }
+
+    // Log when the user chooses to abstain from the survey and proceeds to uninstall
+    public static void deadlineSurveyAbstained() {
+        Map<String, Object> data = new HashMap<>();
+        data.put(DEADLINESURVEY, "Abstained deadline survey");
+        FirebaseWrapper.push(LOGS, data);
+    }
+
+    // Log when the user accepts to complete the survey (does not mean the use *has* submitted it)
+    public static void deadlineSurveyAccepted() {
+        Map<String, Object> data = new HashMap<>();
+        data.put(DEADLINESURVEY, "Accepted deadline survey");
+        FirebaseWrapper.push(LOGS, data);
+    }
+
+    // Log when the user submitted the deadline survey
+    public static void deadlineSurveySubmitted() {
+        Map<String, Object> data = new HashMap<>();
+        data.put(DEADLINESURVEY, "Submitted deadline survey");
+        FirebaseWrapper.push(LOGS, data);
+    }
+
 
 }
