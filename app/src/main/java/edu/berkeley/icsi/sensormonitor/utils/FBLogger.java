c18aebc901fed33d07ca4f6ddaf14c7a2b47e0c5
Manar <safi.manar@berkeley.edu>
2016-10-26 13:21:15 -0700

    FBLogger added ENABLED flag, and permanenlty disabled problematic consent

--- app/src/main/java/edu/berkeley/icsi/sensormonitor/utils/FBLogger.java
+++ app/src/main/java/edu/berkeley/icsi/sensormonitor/utils/FBLogger.java
@@ -28,6 +28,9 @@ public class FBLogger {
     private static final String DAILYSURVEY = "dailysurvey";
     private static final String DEADLINESURVEY = "deadlinesurvey";
 
+    // Enable Flag
+    private static final boolean ENABLED = true; // false for disabled.
+
 
     /****************************************************************************
      *                                                                          *
@@ -64,40 +67,53 @@ public class FBLogger {
 
     // Log when the user is shown the consent activity
     public static void consentActivity() {
-        Map<String, Object> data = new HashMap<>();
-        data.put(CONSENT, "Displayed consent activity");
-        FirebaseWrapper.push(LOGS, data);
+        // consentActivity currently disabled -- FireBase cannot push at
+        // consent activity because Firebase and UUID are not initialized!
+
+        if (false) {
+            Map<String, Object> data = new HashMap<>();
+            data.put(CONSENT, "Displayed consent activity");
+            FirebaseWrapper.push(LOGS, data);
+        }
+
     }
 
     // Log the user's entire SharedPreferences getting the Prefs map from PreferencesWrapper
     public static void sharedprefs() {
-        Map<String, Object> data = new HashMap<>();
+        if (ENABLED) {
+            Map<String, Object> data = new HashMap<>();
 
-        Map<String, ?> prefsMap = PreferencesWrapper.getSharedPrefs();
+            Map<String, ?> prefsMap = PreferencesWrapper.getSharedPrefs();
 
-        for (Map.Entry<String, ?> entry : prefsMap.entrySet()) {
-            data.put(entry.getKey(), entry.getValue());
+            for (Map.Entry<String, ?> entry : prefsMap.entrySet()) {
+                data.put(entry.getKey(), entry.getValue());
+            }
+
+            FirebaseWrapper.push(SPDUMP, data);
         }
 
-        FirebaseWrapper.push(SPDUMP, data);
     }
 
     // Log when the user has restarted the device
     public static void deviceRestarted() {
-        Map<String, Object> data = new HashMap<>();
-        data.put(GENERAL, "Device restarted");
-        FirebaseWrapper.push(LOGS, data);
+        if (ENABLED) {
+            Map<String, Object> data = new HashMap<>();
+            data.put(GENERAL, "Device restarted");
+            FirebaseWrapper.push(LOGS, data);
+        }
     }
 
     // Log when the app has generated a UUID and log that UUID
     public static void generatedUUID(String uuid) {
-        Map<String, Object> data = new HashMap<>();
-        data.put(GENERAL, "UUID has been generated");
-        String uuidlog = "UUID = " + uuid;
-        data.put(GENERAL, uuidlog);
+        if (ENABLED) {
+            Map<String, Object> data = new HashMap<>();
+            data.put(GENERAL, "UUID has been generated");
+            String uuidlog = "UUID = " + uuid;
+            data.put(GENERAL, uuidlog);
 
-        FirebaseWrapper.push(LOGS, data);
-    }
+            FirebaseWrapper.push(LOGS, data);
+        }
+            }
 
 
 
@@ -108,37 +124,52 @@ public class FBLogger {
 
     // Log when the daily survey activity is displayed
     public static void dailySurveyActivity() {
-        Map<String, Object> data = new HashMap<>();
-        data.put(DAILYSURVEY, "Displayed dailysurvey activity");
-        FirebaseWrapper.push(LOGS, data);
+
+        if (ENABLED) {
+            Map<String, Object> data = new HashMap<>();
+            data.put(DAILYSURVEY, "Displayed dailysurvey activity");
+            FirebaseWrapper.push(LOGS, data);
+        }
+
+
     }
 
     // Log when the user postpones the daily survey
     public static void dailySurveyPostponed() {
-        Map<String, Object> data = new HashMap<>();
-        data.put(DAILYSURVEY, "Postponed daily survey");
-        FirebaseWrapper.push(LOGS, data);
+        if (ENABLED) {
+            Map<String, Object> data = new HashMap<>();
+            data.put(DAILYSURVEY, "Postponed daily survey");
+            FirebaseWrapper.push(LOGS, data);
+        }
+
     }
 
     // Log when the user chooses to abstain from the survey
     public static void dailySurveyAbstained() {
-        Map<String, Object> data = new HashMap<>();
-        data.put(DAILYSURVEY, "Abstained daily survey");
-        FirebaseWrapper.push(LOGS, data);
+        if (ENABLED) {
+            Map<String, Object> data = new HashMap<>();
+            data.put(DAILYSURVEY, "Abstained daily survey");
+            FirebaseWrapper.push(LOGS, data);
+        }
     }
 
     // Log when the user accepts to complete the survey (does not mean the use *has* submitted it)
     public static void dailySurveyAccepted() {
-        Map<String, Object> data = new HashMap<>();
-        data.put(DAILYSURVEY, "Accepted daily survey");
-        FirebaseWrapper.push(LOGS, data);
+        if (ENABLED) {
+            Map<String, Object> data = new HashMap<>();
+            data.put(DAILYSURVEY, "Accepted daily survey");
+            FirebaseWrapper.push(LOGS, data);
+        }
+
     }
 
     // Log when the user submitted the daily survey
     public static void dailySurveySubmitted() {
-        Map<String, Object> data = new HashMap<>();
-        data.put(DAILYSURVEY, "Submitted daily survey");
-        FirebaseWrapper.push(LOGS, data);
+        if (ENABLED) {
+            Map<String, Object> data = new HashMap<>();
+            data.put(DAILYSURVEY, "Submitted daily survey");
+            FirebaseWrapper.push(LOGS, data);
+        }
     }
 
 
@@ -147,37 +178,47 @@ public class FBLogger {
      ****************************************************************************/
     // Log when the deadline activity is displayed
     public static void deadlineActivity() {
-        Map<String, Object> data = new HashMap<>();
-        data.put(DEADLINESURVEY, "Displayed deadline activity");
-        FirebaseWrapper.push(LOGS, data);
+        if (ENABLED) {
+            Map<String, Object> data = new HashMap<>();
+            data.put(DEADLINESURVEY, "Displayed deadline activity");
+            FirebaseWrapper.push(LOGS, data);
+        }
     }
 
     // Log when the user postpones the deadline survey
     public static void deadlineSurveyPostponed() {
-        Map<String, Object> data = new HashMap<>();
-        data.put(DEADLINESURVEY, "Postponed deadline survey");
-        FirebaseWrapper.push(LOGS, data);
+        if (ENABLED) {
+            Map<String, Object> data = new HashMap<>();
+            data.put(DEADLINESURVEY, "Postponed deadline survey");
+            FirebaseWrapper.push(LOGS, data);
+        }
     }
 
     // Log when the user chooses to abstain from the survey and proceeds to uninstall
     public static void deadlineSurveyAbstained() {
-        Map<String, Object> data = new HashMap<>();
-        data.put(DEADLINESURVEY, "Abstained deadline survey");
-        FirebaseWrapper.push(LOGS, data);
+        if (ENABLED) {
+            Map<String, Object> data = new HashMap<>();
+            data.put(DEADLINESURVEY, "Abstained deadline survey");
+            FirebaseWrapper.push(LOGS, data);
+        }
     }
 
     // Log when the user accepts to complete the survey (does not mean the use *has* submitted it)
     public static void deadlineSurveyAccepted() {
-        Map<String, Object> data = new HashMap<>();
-        data.put(DEADLINESURVEY, "Accepted deadline survey");
-        FirebaseWrapper.push(LOGS, data);
+        if (ENABLED) {
+            Map<String, Object> data = new HashMap<>();
+            data.put(DEADLINESURVEY, "Accepted deadline survey");
+            FirebaseWrapper.push(LOGS, data);
+        }
     }
 
     // Log when the user submitted the deadline survey
     public static void deadlineSurveySubmitted() {
-        Map<String, Object> data = new HashMap<>();
-        data.put(DEADLINESURVEY, "Submitted deadline survey");
-        FirebaseWrapper.push(LOGS, data);
+        if (ENABLED) {
+            Map<String, Object> data = new HashMap<>();
+            data.put(DEADLINESURVEY, "Submitted deadline survey");
+            FirebaseWrapper.push(LOGS, data);
+        }
     }
 
 
