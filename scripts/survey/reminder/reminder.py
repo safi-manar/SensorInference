12eaa81ff23e7dc83ffc42423ef1457f646ef1de
ioreyes <ioreyes@icsi.berkeley.edu>
2016-11-19 16:47:15 -0500

    Completed the survey reminder-scheduling script

--- scripts/survey/reminder/reminder.py
+++ scripts/survey/reminder/reminder.py
@@ -14,7 +14,7 @@ def parse_args():
 
     script_dir = os.path.dirname(os.path.realpath(__file__))
     default_sched = os.path.join(script_dir, 'schedule.json')
-    parser.add_argument('-s', '--schedule-file', help='path to load/save a file containing the reminder schedule (default: <script-dir>/schedule.json', default=default_sched)
+    parser.add_argument('-s', '--schedule-path', help='path to load/save a file containing the reminder schedule (default: <script-dir>/schedule.json', default=default_sched)
 
     return parser.parse_args()
 
@@ -26,8 +26,8 @@ def read_json(path):
 # Get and check args
 args = parse_args()
 
-schedule = args.schedule_file
-work_days = args.days
+schedule_path = args.schedule_path
+work_days = max(0, args.days - 1)   # Subtract becasue no need to remind on the first day
 api_path = args.api_path if args.api_path != None and os.path.isfile(args.api_path) else None
 if api_path != None:
     print 'Using API file "%s"' % api_path
@@ -57,20 +57,42 @@ else:
 hit_id = hit.HITId
 approved = [asgn for asgn in mtk.get_assignments(hit_id, page_size=100) if asgn.AssignmentStatus == 'Approved']
 
+# Load an existing schedule, if it exists
+schedule = read_json(schedule_path) if os.path.isfile(schedule_path) else {}
+
 # Issue out reminders 12 hours after submission, then every 24 after that, until the end of the observation period
 current = datetime.now()
 submission_offset = timedelta(hours=12)
 reminder_period = timedelta(hours=24)
 for appr in approved:
-    submit_time = datetime.strptime(appr.SubmitTime, '%Y-%m-%dT%H:%M:%SZ')
-    reminder_time = submit_time + submission_offset
-    while reminder_time + reminder_period < current:
-        reminder_time = reminder_time + reminder_period
+    worker = appr.WorkerId
+    assignment = appr.AssignmentId
 
-    print reminder_time
+    if worker in schedule.keys(): # Read and process any existing schedule
+        worker_schedule = schedule[worker]
+        for rem_str in worker_schedule.keys():
+            if worker_schedule[rem_str] == 0 and datetime.strptime(rem_str, '%Y-%m-%dT%H:%M:%S') < current:
+                worker_schedule[rem_str] = 1
 
-print schedule
+                # Issue reminder
+                print 'Reminding %s' % worker
+                mtk.notify_workers(worker, 'Reminder for "%s"' % target_hit_title, \
+                                   '''This is a friendly reminder from the ICSI/UC Berkeley Sensors Inference team to fill out the activity survey, available through our app at approximately 8:00 PM in your timezone.  Please don\'t hesitate to contact our team at sensor-inference@icsi.berkeley.edu if you have any questions. Many thanks!''')
+                break
 
+    else:    # Generate a reminder schedule if there isn't already one
+        submit_time = datetime.strptime(appr.SubmitTime, '%Y-%m-%dT%H:%M:%SZ')
+        reminder = submit_time + submission_offset
 
+        worker_schedule = {}
+        for n in range(work_days):
+            worker_schedule[reminder.isoformat()] = 0 if reminder > current else 1  # 1 if already past due, 0 otherwise
+            reminder = reminder + reminder_period
 
+        schedule[worker] = worker_schedule
+
+# Write out the (new or edited) schedule
+with open(schedule_path, 'w') as outfile:
+    json.dump(schedule, outfile, sort_keys=True, indent=4)
+    print 'Writing schedule to %s' % schedule_path
 
