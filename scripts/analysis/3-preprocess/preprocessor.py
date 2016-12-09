d894b943599fbd74c3e417acf2605c053987eff2
Irwin Reyes <ioreyes@icsi.berkeley.edu>
2016-12-09 10:22:42 -0800

    Improved preprocessing efficiency by using pandas module

--- scripts/analysis/3-preprocess/preprocessor.py
+++ scripts/analysis/3-preprocess/preprocessor.py
@@ -1,5 +1,6 @@
 import os
 import csv
+import pandas
 
 class Preprocessor:
     'Preprocessing operations for CVS\'d Firebase data'
@@ -20,18 +21,20 @@ class Preprocessor:
     def run(self, out_path):
         good_columns = self._column_check()
         if(good_columns):
-            # Read in file
-            infile = open(self._csv_path, 'r')
-            data = csv.DictReader(infile)
+            # Read in file and desired headers
+            data = pandas.read_csv(self._csv_path)
+            headers = self._get_renamed_headers()
 
             # Preprocessing operations
-            sorted_data = self._sort_by_first_col(data)
+            data = data[self._columns_list]                             # Reorder columns
+            data.columns = headers                                      # Rename columns
+            data = data.sort_values(by=headers[0], ascending=[True])    # Sort by first column
 
             # Write out the file
             out_file = os.path.join(out_path, self._filename)
             if os.path.isfile(out_file):
                 out_file = out_file + '.pprc'   # Add extension to avoid overwrites
-            self._write_out(sorted_data, out_file)
+            data.to_csv(path_or_buf=out_file, header=headers, index=False)
 
     def _column_check(self, in_path=None):
         """Ensure that the source CSV has the expected number of columns with the right headers"""
@@ -44,15 +47,7 @@ class Preprocessor:
 
             return len(headers_list) == len(self._columns_list) and len([x for x in headers_list if x not in self._columns_list]) == 0
 
-    def _sort_by_first_col(self, csv_data):
-        """Sort CSV data by whatever's in the first row of the desired columns ordering"""
-        col_label = self._columns_list[0]
-
-        return sorted(csv_data, key=lambda row: row[col_label])
-
-    def _write_out(self, csv_data, out_path):
-        """Write out the file in the ordering specified by the columns list"""
-
+    def _get_renamed_headers(self):
         # Get the updated headers, if available
         headers = []
         if self._rename_map != None:
@@ -64,11 +59,4 @@ class Preprocessor:
         else:
             headers = self._columns_list
 
-        with open(out_path, 'w') as outfile:
-            # Write the headers first
-            outfile.write(','.join(headers) + '\n')
-
-            # Then the data
-            writer = csv.DictWriter(outfile, fieldnames=self._columns_list)
-            for row in csv_data:
-                writer.writerow(row)
+        return headers
