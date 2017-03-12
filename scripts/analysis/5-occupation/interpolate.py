1a994f4fcf6b2e281beff18033784988835e635f
Manar Safi <safi.manar@berkeley.edu>
2017-03-11 20:04:19 -0800

    Refactored to column-wise for efficiency.

--- scripts/analysis/5-occupation/interpolate.py
+++ scripts/analysis/5-occupation/interpolate.py
@@ -7,20 +7,14 @@ import pandas as pd
 import numpy as np
 from multiprocessing import Pool # [2]
 
-NUM_PARTITIONS = 8
+NUM_PARTITIONS = 8 # For parallelization
 
 def interpolate(accel, PARALLEL=False, CORES=4):
     columns = accel.columns # backup the original columns
     accel = trim(accel)
-    if (PARALLEL):
-        accel = cleanTimeParallel(accel, CORES)
-    else:
-        accel = cleanTime(accel)
+    accel = cleanTime(accel)
     #accel = L2(accel) # Add the L2 column.
     return accel
-
-
-
 
 
 # Keep only the relevant columns for this script for efficiency
@@ -34,35 +28,16 @@ def trim(accel):
 def cleanTime(accel):
     print("PreTest")
     print(accel.columns.tolist())
-    accel.apply(normalize, axis=1) # Apply normalize row-wise.
+    accel['time'] = accel['t'] # Duplicate the time
+    accel['time'] = accel['time'].apply(normalize) # Apply normalize row-wise.
     print("PostTest")
     print(accel.columns.tolist())
     accel = accel[['t', 'time', 'x', 'y', 'z']]# TODO Delete
     return accel
-
-# At 50Hz, there should be an event every 20ms.
-def cleanTimeParallel(accel, CORES):
-    print("PreTest: Parallel")
-    print(accel.columns.tolist())
-    accel_split = np.array_split(accel, NUM_PARTITIONS)
-    pool = Pool(CORES)
-    accel = pd.concat(pool.map(normalizeFunc, accel_split))
-    pool.close()
-    pool.join()
-    print("PostTest")
-    print(accel.columns.tolist())
-    accel = accel[['t', 'time', 'x', 'y', 'z']]# TODO Delete
-    return accel
-
-def normalizeFunc(df):
-    return df.apply(normalize, axis=1)
 
 # Round the time value to the nearest 20 [1]
-def normalize(row):
-    time = row['t']
-    row['time'] = round(time / 20) * 20 # TODO Change 'time' --> 't'
-    return row
-
+def normalize(time):
+    return int(round(float(time) / 20) * 20)
 
 
 def L2(accel):
