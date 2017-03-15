9e2f2bef730213052308a60cde0a742cd52ca76e
Manar Safi <safi.manar@berkeley.edu>
2017-03-14 23:24:13 -0700

    Refactored to use Pandas Series native interpolation.

--- scripts/analysis/5-occupation/interpolate.py
+++ scripts/analysis/5-occupation/interpolate.py
@@ -9,51 +9,61 @@ from multiprocessing import Pool # [2]
 
 NUM_PARTITIONS = 8 # For parallelization
 
+
 def interpolate(accel, PARALLEL=False, CORES=4):
-    columns = accel.columns # backup the original columns
     accel = trim(accel)
-    accel = cleanTime(accel)
-    accel = L2(accel) # Add the L2 column.
-    return accel
+    accel = L2(accel)
+    accel = accel.loc[:, ['t', 'L2']] # More trimming.
+    ts = getTimeSeries(accel) # A time series for L2 data.
+    rs = getResampledSeries(accel) # A time index
+    ip = interpFromIndex(ts, rs)
+    return ip
 
 
-# Keep only the relevant columns for this script for efficiency
+
+# For efficiency, keep only the relevant columns.
 def trim(accel):
     accel = accel.loc[:, ['timestamp', 'x-minus-gx', 'y-minus-gy', 'z-minus-gz']]
     accel.columns = ['t', 'x', 'y', 'z']
     return accel
-
-
-# At 50Hz, there should be an event every 20ms.
-def cleanTime(accel):
-    print("PreTest")
-    print(accel.columns.tolist())
-    accel['time'] = accel['t'] # Duplicate the time
-    accel['time'] = accel['time'].apply(normalize) # Apply normalize row-wise.
-    print("PostTest")
-    print(accel.columns.tolist())
-    accel = accel[['t', 'time', 'x', 'y', 'z']]# TODO Delete
-    return accel
-
-# Round the time value to the nearest 20 [1]
-def normalize(time):
-    return int(round(float(time) / 20) * 20)
-
 
 def L2(accel):
     accel[['x', 'y', 'z']] = np.square(accel[['x', 'y', 'z']])
     # Sum the rows into a new column
     accel['sum'] = accel['x'] + accel['y'] + accel['z']
     accel['sum'] = np.sqrt(accel['sum'])
-    accel.columns = ['t', 'time', 'x', 'y', 'z', 'L2'] # Rename sum
+    accel.columns = ['t', 'x', 'y', 'z', 'L2'] # Rename sum
     return accel
 
-# The linear interpolation between two points.
-def interp(x0, y0, x1, y1, x):
-    return y0 + ((x - x0) * (y1 - y0) / (x1 - x0))
+# Returns a time series for the L2 data, using time as the index.
+def getTimeSeries(accel):
+    times = pd.to_datetime(accel['t'], unit='ms') # Create a DateTimeIndex series from 't'
+    data = accel['L2'] # Create a series from the L2 data.
+    return pd.Series(data.values, times) # Create a Series from L2 data and using times as the index
+
+# Given the accelerometer data, return a time seriess index with the times snapped to the nearest 20ms.
+def getResampledSeries(accel):
+    snap = 20 # Interval in milliseconds.
+    accel['t'] = accel['t'].apply(normalize) # Normalize the times.
+    rts = getTimeSeries(accel)
+    rs = pd.Series(index=rts.index) # Replaces L2 values with NaN, keeping only the time.
+    return rs # return only the index.
+
+# Round the time value to the nearest 20 [1]
+def normalize(time):
+    return int(round(float(time) / 20) * 20)
+
+# Given time series ts = with L2 data and time as the index, and
+# time series index rs, return the interpolated values of rs. From [3]
+def interpFromIndex(ts, rs):
+    ip = pd.concat([ts, rs]).sort_index()
+    ip = ip.interpolate()
+    return ip[rs.index] # Return only those values that were in rs.
+
 
 
 
 
 # [1] http://stackoverflow.com/questions/9810391/round-to-the-nearest-500-python
 # [2] http://www.racketracer.com/2016/07/06/pandas-in-parallel/
+# [3] http://stackoverflow.com/questions/18955250/interpolating-one-time-series-onto-another-in-pandas
